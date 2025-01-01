ec17640e7e018506cae33f8eebb64303a9d483d9
Farahshabir <farah.shabir@hotmail.com>
2025-01-01 19:27:01 +0500

    first commit

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -68,9 +68,74 @@ export default function Cart() {
         {/* Header and Cart Empty State */}
         <header className="shadow-lg font-[sans-serif] tracking-wide relative z-50">
           <div id="collapseMenu" className="max-lg:hidden lg:!block">
-            <ul className="lg:flex lg:items-center px-10 py-3 bg-[#333] min-h-[46px] gap-4">
-              {/* Menu Links */}
-            </ul>
+          <ul className="lg:flex lg:items-center px-10 py-3 bg-[#333] min-h-[46px] gap-4">
+            <li>
+              <Link
+                href="/"
+                className="hover:text-yellow-300 text-yellow-300 text-[15px] font-medium"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/shops"
+                className="hover:text-yellow-300 text-white text-[15px] font-medium"
+              >
+                Shop
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/about"
+                className="hover:text-yellow-300 text-white text-[15px] font-medium"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/blog"
+                className="hover:text-yellow-300 text-white text-[15px] font-medium"
+              >
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/contact"
+                className="hover:text-yellow-300 text-white text-[15px] font-medium"
+              >
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/pages"
+                className="hover:text-yellow-300 text-white text-[15px] font-medium"
+              >
+                Pages
+              </Link>
+            </li>
+            <li className="max-lg:py-2 px-4 cursor-pointer">
+              <span className="relative">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6 inline"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    d="M164.96 300.004h.024c.02 0 .04-.004.059-.004H437a15.003 15.003 0 0 0 14.422-10.879l60-210a15.003 15.003 0 0 0-2.445-13.152A15.006 15.006 0 0 0 497 60H130.367l-10.722-48.254A15.003 15.003 0 0 0 105 0H15C6.715 0 0 6.715 0 15s6.715 15 15 15h77.969c1.898 8.55 51.312 230.918 54.156 243.71C131.184 280.64 120 296.536 120 315c0 24.812 20.188 45 45 45h272c8.285 0 15-6.715 15-15s-6.715-15-15-15H165c-8.27 0-15-6.73-15-15 0-8.258 6.707-14.977 14.96-14.996zM477.114 90l-51.43 180H177.032l-40-180zM150 405c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm167 15c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm0 0"
+                    data-original="#000000"
+                  ></path>
+                </svg>
+                <span className="absolute left-auto -ml-1 -top-2 rounded-full bg-red-500 px-1 py-0 text-xs text-white">
+                  {totalCartItems}
+                </span>
+              </span>
+            </li>
+          
+          </ul>
           </div>
         </header>
         <div className="font-sans">
@@ -172,7 +237,7 @@ export default function Cart() {
                   ></path>
                 </svg>
                 <span className="absolute left-auto -ml-1 -top-2 rounded-full bg-red-500 px-1 py-0 text-xs text-white">
-                  {/* {totalCartItems} */}
+                  {totalCartItems}
                 </span>
               </span>
             </li>
